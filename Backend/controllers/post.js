b2df031dbcd44dba4b31493122d972818aff3d6f
mosin74 <skmosin70201566@gmail.com>
2024-05-11 21:08:10 +0530

    LikeDislikepost and deletPost

--- Backend/controllers/post.js
+++ Backend/controllers/post.js
@@ -36,3 +36,105 @@ exports.createPost = async (req, res) => {
         })
     }
 }
+
+
+
+
+exports.likeDislikepost=async(req,res)=>{
+
+    try {
+    const post=await Post.findById(req.params.id);
+    console.log(post);
+
+        if(!post){
+         return res.status(404).json({
+             sucess:false,
+             message:"post not found"
+         });
+     }
+     //Why   ==> user._id 
+    if(post.likes.includes(req.user._id)){
+         const index=post.likes.indexOf(req.user._id)
+         post.likes.splice(index,1);
+         await post.save()
+         return res.status(200).json({
+             success:true,
+             message:"Post disliked"
+         })
+    }
+    else{
+         post.likes.push(req.user._id);
+         await post.save();
+         return res.status(200).json({
+             success:true,
+             message:"Post liked"
+         })
+     
+    }
+        
+    } catch (error) {
+        return res.status(500).json({
+            success:false,
+            message:error.message
+        })
+    }
+   
+}
+
+
+
+
+exports.deletPost=async (req,res)=>{
+try {
+    const post = await Post.findById(req.params.id);
+
+
+    if(!post){
+
+        return res.status(404).json({
+            sucess:false,
+            message:"post not found"
+        })
+    }
+
+
+    if(post.Owner.toString()!=req.user._id.toString()){
+        return res.status(401).json({
+            sucess:false,
+            message:"Unauthorized"
+        })
+    }
+
+ 
+   
+        // await port.remove({})//this method is no more in moongoose new version changed to deleteOne or deleteMany
+        await post.deleteOne()
+
+
+        const user=await User.findById(req.user._id)
+        const index=user.posts.indexOf(req.params.id)
+        
+
+        
+// ==>We can use any one of them for removing from user posts array 
+
+        // await user.posts.pull({_id:req.params.id})
+        await user.posts.splice(index,1)
+
+
+        await user.save()
+
+        return res.status(200).json({
+            success:true,
+            message:"Post deleted",
+            user
+        })
+    
+} catch (error) {
+    return res.status(500).json({
+        sucess:false,
+        message:error.message
+    })
+}
+   
+}
